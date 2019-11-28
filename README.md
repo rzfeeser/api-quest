415182fb9ebdb31716adba5129b7164df298de0c
rzfeeser <rzfeeser@users.noreply.github.com>
2019-11-28 01:54:11 -0500

    improvements in stablility

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # api-quest
 A RPG written in Python 3.7, played across a Flask API framework
+
+### Git Branches
+- master - Latest stable version
+- devel - Under development
+- stable-1.0 - First Stable Release (Nov. 2019)
--- rpgserver.py
+++ rpgserver.py
@@ -1,13 +1,22 @@
 #!/usr/bin/python3
 """Welcome to Alta3 Web RPG"""
 
+## standard library
 import json
+
+## flask imports
 from flask import Flask
 from flask import session
 from flask import render_template
 from flask import redirect
 from flask import url_for
 from flask import request
+
+## parsing user commands
+# parse out get commands
+from usercommands import get
+# parse out go commands
+from usercommands import go
 
 """
 Flask Sessions are leveraged to store encrypted cookie data, the player's state, on the client side.
@@ -23,58 +32,16 @@ API endpoints:
    /get/<item> - attempt to pick up an item
 
    /help/ - return help
-
-   /look/ - return what is in the room, an abbreviated form of /status/
+   
 """
 
-
 # create the world object
-with open ("world.json", "r") as world:
+with open("world.json", "r") as world:
     rooms = json.load(world)
 
 
 app = Flask(__name__)
 app.secret_key = "rzfeeser:RAND"
-
-# user types help
-def gamehelp():
-    return '''
-    The objective of the game is to grab the key and the potion, then escape through the garden!
-    Try not to get eaten by a grue.
-
-    RPG GAME
-    ========
-    Commands:
-      go [direction]
-      get [item]
-      help
-    '''
-
-# user types look
-# user type attack
-# user types use
-# user types drop
-
-
-# user types go
-def go(direction, currentRoom):
-    if direction in rooms[currentRoom]:
-        session["currentRoom"] = rooms[currentRoom][direction]
-        return f"You moved {direction}."
-    else:
-        return f"You cannot move {direction}."
-
-
-# user types get kitkat (user tries to pick something up)
-def get(kitkat, currentRoom):
-    if 'item' in session.get('rooms')[currentRoom] and kitkat in session.get('rooms')[currentRoom]['item']:
-        session['inventory'] += [kitkat]
-        session['rooms'][currentRoom].pop('item', None)
-        session.modified = True
-#        return session['rooms'][currentRoom] #.pop('item', None)
-        return f"{kitkat} picked up!"
-    else:
-        return f"You cannot pickup {kitkat}."
 
 
 # start a new game
@@ -98,10 +65,8 @@ def new():
 # always called at start of turn
 @app.route("/status/", methods = ["POST", "GET"])
 def status():
-
     if request.method == "GET":
         status = {}
-
         # Return your current room
         # check if user had a new game spawned or they cut right to /status/
         if session.get('currentRoom'):
@@ -118,17 +83,19 @@ def status():
         if session.get("currentRoom") == "Garden":
             if 'skeleton key' in session.get('inventory') and 'potion' in session.get('inventory'):
                 return redirect(url_for("gameover", endreason="""You enter the garden, and unlock the rusty gate with
-                the skeleton key!"""))
+                the skeleton key!\nYou win!"""))
             else:
                 session["turnresult"] = "You'd win if you only had a few more items..."
-
-
 
         # An item is in the room you are in
         if 'item' in session.get('rooms')[session.get('currentRoom')]:
             status['item'] = session.get('rooms')[(session['currentRoom'])]['item'] # set it within the json
             ## monster check
-            if status['item'] == 'monster':
+            if status['item'] == 'monster' and "spell book" in session.get('inventory'):
+                session['rooms'][session.get('currentRoom')].pop('item', None) # remove the key item from dictionary
+                session['inventory'].remove("spell book") # remove the item "spell book" from the list
+                session['turnresult'] = "You walk into a dark room. A grue is here. As it approaches you, the spell book falls from your pouch and opens to a page with charcoal drawings of cryptic runes and a drawing of a latern. Bright light suddenly beams from the book eviserating the Grue."
+            elif status['item'] == 'monster':
                 return redirect(url_for("gameover", endreason="""There is a hungry Grue here! And you're invited to be
                 dinner!"""))
         else:
@@ -150,7 +117,7 @@ def status():
             playermove = playermove.lower().split(" ", 1) # only split on the first whitespace
 
             if playermove[0] == "go": # if the user typed 'go'
-                session["turnresult"] = go(playermove[1], session.get("currentRoom"))
+                session["turnresult"] = go(rooms, playermove[1], session.get("currentRoom"))
             elif playermove[0] == "get": # if the user typed 'get'
                 session["turnresult"] = get(playermove[1], session.get("currentRoom"))
             elif playermove[0] == "help": # if the user typed 'help'
@@ -168,7 +135,10 @@ def status():
 def gameover(endreason):
     if request.method == "GET":
         session.clear()  # delete the user's session cookie
-        return render_template("gameover.html", endreason=endreason)
+        if "win" in endreason:
+            return render_template("win.html", endreason=endreason)
+        else:
+            return render_template("gameover.html", endreason=endreason)
 
 if __name__ == "__main__":
     app.run(port=5006)
