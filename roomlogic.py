8ca306dbf81bb4bda29846c9c4865e72e6ba2bca
RZFeeser <rzfeeser@users.noreply.github.com>
2025-09-23 03:24:12 +0000

    fix update

--- roomlogic.py
+++ roomlogic.py
@@ -58,3 +58,8 @@ def roomlogic(currentroom):
         return library
     elif currentroom == "kitchen":
         return kitchen
+    else:
+        def noop(session):
+            #session["turnresult"] = f"You are in {currentroom}. There is nothing special here."
+            return
+        return noop
